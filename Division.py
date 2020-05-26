f1bd6c8bf846f391f587ebce1d96c4a5341f0c7d
Aastha Chandok <61003029+aasthachandokk@users.noreply.github.com>
2020-05-26 17:13:59 +0530

    Update Division.py

--- Division.py
+++ Division.py
@@ -1,5 +1,5 @@
 #Task
-#Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+#Read two integers and print two lines. The first line should contain integer division,  a//b . The second line should contain float division,  a/b .
 #You don't need to perform any rounding or formatting operations.
 
 #Input Format
